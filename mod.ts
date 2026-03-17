3eeaa2e9e742dc24b51aaf1ef7ae4085e2a3a160
Garrett HE <garrett.he@outlook.com>
2026-03-17 12:21:53 +0800

    feat: add configurations to specify vendors

--- mod.ts
+++ mod.ts
@@ -1,146 +1,112 @@
-const miscItems = {
-    potions: ["Full Rejuvenation Potion"],
-    gems: [
-        "Chipped Amethyst", "Flawed Amethyst", "Amethyst", "Flawless Amethyst", "Perfect Amethyst",
-        "Chipped Topaz", "Flawed Topaz", "Topaz", "Flawless Topaz", "Perfect Topaz",
-        "Chipped Sapphire", "Flawed Sapphire", "Sapphire", "Flawless Sapphire", "Perfect Sapphire",
-        "Chipped Emerald", "Flawed Emerald", "Emerald", "Flawless Emerald", "Perfect Emerald",
-        "Chipped Ruby", "Flawed Ruby", "Ruby", "Flawless Ruby", "Perfect Ruby",
-        "Chipped Diamond", "Flawed Diamond", "Diamond", "Flawless Diamond", "Perfect Diamond",
-        "Chipped Skull", "Flawed Skull", "Skull", "Flawless Skull", "Perfect Skull"
-    ],
-    charms: ["Small Charm", "Large Charm", "Grand Charm"],
-    runes: [
-        "El Rune", "Eld Rune", "Tir Rune",
-        "Nef Rune", "Eth Rune", "Ith Rune",
-        "Tal Rune", "Ral Rune", "Ort Rune",
-        "Thul Rune", "Amn Rune", "Sol Rune",
-        "Shael Rune", "Dol Rune", "Hel Rune",
-        "Io Rune", "Lum Rune", "Ko Rune",
-        "Fal Rune", "Lem Rune", "Pul Rune",
-        "Um Rune", "Mal Rune", "Ist Rune",
-        "Gul Rune", "Vex Rune", "Ohm Rune",
-        "Lo Rune", "Sur Rune", "Ber Rune",
-        "Jah Rune", "Cham Rune", "Zod Rune",
-    ],
-    jewels: ["Ring", "Amulet", "Jewel"]
-};
-
-const classItems = {
-    amazon: ["Stag Bow", "Reflex Bow", "Maiden Spear", "Maiden Pike", "Maiden Javelin"],
-    assassin: ["Katar", "Wrist Blade", "Hatchet Hands", "Cestus", "Claws", "Blade Talons", "Scissors Katar"],
-    barbarian: ["Jawbone Cap", "Fanged Helm", "Horned Helm", "Assault Helmet", "Avenger Guard"],
-    druid: ["Totemic Mask", "Blood Spirit", "Sky Spirit", "Hunter's Guise"],
-    paladin: ["Targe", "Rondache", "Heraldic Shield", "Aerin Shield", "Crown Shield"],
-    necromancer: ["Preserved Head", "Zombie Head", "Unraveller Head", "Gargoyle Head", "Demon Head"],
-    sorceress: ["Eagle Orb", "Sacred Globe", "Smoked Sphere", "Clasped Orb", "Jared's Stone"],
-    warlock: ["Old Book", "Tome", "Codex", "Compendium", "Grimoire"]
-};
-
-const vendorSpec = [
+const FILE_SPECS = [
     {
-        filename: "global\\excel\\misc.txt",
-        vendors: [
-            // Act 1
-            {name: "Akara", items: [...miscItems.potions, ...miscItems.gems, ...miscItems.charms, ...miscItems.runes, ...miscItems.jewels]},
-
-            // Act 2
-            {name: "Drognan", items: [...miscItems.potions, ...miscItems.gems, ...miscItems.charms, ...miscItems.runes, ...miscItems.jewels]},
-
-            // Act 3
-            {name: "Ormus", items: [...miscItems.potions, ...miscItems.gems, ...miscItems.charms, ...miscItems.runes, ...miscItems.jewels]},
-
-            // Act 4
-            {name: "Jamella", items: [...miscItems.potions, ...miscItems.gems, ...miscItems.charms, ...miscItems.runes, ...miscItems.jewels]},
-
-            // Act 5
-            {name: "Malah", items: [...miscItems.potions, ...miscItems.gems, ...miscItems.charms, ...miscItems.runes, ...miscItems.jewels]}
-        ]
+        files: ["global\\excel\\misc.txt", "global\\excel\\base\\misc.txt"],
+        groups: ["Potions", "Charms", "Gems", "Runes", "Jewels"]
     },
     {
-        filename: "global\\excel\\armor.txt",
-        vendors: [
-
-            // Act 1
-            {name: "Akara", clear: true, items: [...classItems.barbarian, ...classItems.druid, ...classItems.paladin]},
-            {name: "Gheed", clear: true, items: [...classItems.necromancer, ...classItems.warlock]},
-
-            // Act 2
-            {name: "Drognan", clear: true, items: [...classItems.barbarian, ...classItems.druid, ...classItems.paladin]},
-            {name: "Lysander", clear: true, items: [...classItems.necromancer, ...classItems.warlock]},
-
-            // Act 3
-            {name: "Ormus", clear: true, items: [...classItems.barbarian, ...classItems.druid, ...classItems.paladin]},
-            {name: "Alkor", clear: true, items: [...classItems.necromancer, ...classItems.warlock]},
-
-            // Act 4
-            {name: "Jamella", clear: true, items: [...classItems.barbarian, ...classItems.druid, ...classItems.paladin]},
-            {name: "Halbu", clear: true, items: [...classItems.necromancer, ...classItems.warlock]},
-
-            // Act 5
-            {name: "Malah", clear: true, items: [...classItems.barbarian, ...classItems.druid, ...classItems.paladin]},
-            {name: "Larzuk", clear: true, items: [...classItems.necromancer, ...classItems.warlock]},
-        ]
+        files: ["global\\excel\\weapons.txt", "global\\excel\\base\\weapons.txt"],
+        groups: ["AmazonWeapons", "AssassinKatars", "SorceressOrbs"]
     },
     {
-        filename: "global\\excel\\weapons.txt",
-        vendors: [
-            // Act 1
-            {name: "Gheed", clear: true, items: [...classItems.amazon, ...classItems.assassin, ...classItems.sorceress]},
-
-            // Act 2
-            {name: "Lysander", clear: true, items: [...classItems.amazon, ...classItems.assassin, ...classItems.sorceress]},
-
-            // Act 3
-            {name: "Alkor", clear: true, items: [...classItems.amazon, ...classItems.assassin, ...classItems.sorceress]},
-
-            // Act 4
-            {name: "Jamella", clear: true, items: [...classItems.amazon, ...classItems.assassin, ...classItems.sorceress]},
-
-            // Act 5
-            {name: "Malah", clear: true, items: [...classItems.amazon, ...classItems.assassin, ...classItems.sorceress]},
-        ]
+        files: ["global\\excel\\armor.txt", "global\\excel\\base\\armor.txt"],
+        groups: ["BarbarianHelms", "DruidPelts", "PaladinShields", "NecromancerHeads", "WarlockGrimoires"]
     }
 ];
 
-function assignVendor(row, vendor: string) {
-    row["spawnable"] = 1;
-    row["cost"] = 1;
-    row[`${vendor}Min`] = 1;
-    row[`${vendor}Max`] = 1;
-    row[`${vendor}MagicMin`] = 1;
-    row[`${vendor}MagicMax`] = 1;
-    row["PermStoreItem"] = 1;
-    row["multibuy"] = 1;
-}
+const GEMS_SPECS = {
+    chipped: ["Chipped Amethyst", "Chipped Topaz", "Chipped Sapphire", "Chipped Emerald", "Chipped Ruby", "Chipped Diamond", "Chipped Skull"],
+    flawed: ["Flawed Amethyst", "Flawed Topaz", "Flawed Sapphire", "Flawed Emerald", "Flawed Ruby", "Flawed Diamond", "Flawed Skull"],
+    standard: ["Amethyst", "Topaz", "Sapphire", "Emerald", "Ruby", "Diamond", "Skull"],
+    flawless: ["Flawless Amethyst", "Flawless Topaz", "Flawless Sapphire", "Flawless Emerald", "Flawless Ruby", "Flawless Diamond", "Flawless Skull"],
+    perfect: ["Perfect Amethyst", "Perfect Topaz", "Perfect Sapphire", "Perfect Emerald", "Perfect Ruby", "Perfect Diamond", "Perfect Skull"]
+};
 
-function revokeVendor(row, vendor: string) {
-    row[`${vendor}Min`] = "";
-    row[`${vendor}Max`] = "";
-    row[`${vendor}MagicMin`] = "";
-    row[`${vendor}MagicMax`] = "";
-}
+const ITEM_SPECS = {
+    Potions: ["Full Rejuvenation Potion"],
+    Charms: ["Small Charm", "Large Charm", "Grand Charm"],
+    Gems: [],
+    Runes: [
+        "El Rune", "Eld Rune", "Tir Rune", "Nef Rune", "Eth Rune", "Ith Rune",
+        "Tal Rune", "Ral Rune", "Ort Rune", "Thul Rune", "Amn Rune", "Sol Rune",
+        "Shael Rune", "Dol Rune", "Hel Rune", "Io Rune", "Lum Rune", "Ko Rune",
+        "Fal Rune", "Lem Rune", "Pul Rune", "Um Rune", "Mal Rune", "Ist Rune",
+        "Gul Rune", "Vex Rune", "Ohm Rune", "Lo Rune", "Sur Rune", "Ber Rune",
+        "Jah Rune", "Cham Rune", "Zod Rune",
+    ],
+    Jewels: ["Amulet", "Ring", "Jewel"],
+    AmazonWeapons: ["Stag Bow", "Reflex Bow", "Maiden Spear", "Maiden Pike", "Maiden Javelin"],
+    AssassinKatars: ["Katar", "Wrist Blade", "Hatchet Hands", "Cestus", "Claws", "Blade Talons", "Scissors Katar"],
+    BarbarianHelms: ["Jawbone Cap", "Fanged Helm", "Horned Helm", "Assault Helmet", "Avenger Guard"],
+    DruidPelts: ["Totemic Mask", "Blood Spirit", "Sky Spirit", "Hunter's Guise"],
+    PaladinShields: ["Targe", "Rondache", "Heraldic Shield", "Aerin Shield", "Crown Shield"],
+    NecromancerHeads: ["Preserved Head", "Zombie Head", "Unraveller Head", "Gargoyle Head", "Demon Head"],
+    SorceressOrbs: ["Eagle Orb", "Sacred Globe", "Smoked Sphere", "Clasped Orb", "Jared's Stone"],
+    WarlockGrimoires: ["Old Book", "Tome", "Codex", "Compendium", "Grimoire"]
+};
 
-vendorSpec.forEach(spec => {
-    const content = D2RMM.readTsv(spec.filename);
+Object.keys(GEMS_SPECS).forEach(group => {
+    if (config[`${group}GemsEnabled`]) {
+        ITEM_SPECS["Gems"] = ITEM_SPECS["Gems"].concat(GEMS_SPECS[group]);
+    }
+});
 
-    spec.vendors.forEach(vendor => {
-        if (vendor.clear) {
-            content.rows.forEach(row => {
-                revokeVendor(row, vendor.name);
+FILE_SPECS.forEach(fileSpec => {
+    fileSpec.files.forEach(filename => {
+        const content = D2RMM.readTsv(filename);
+        const vendors = {};
+
+        for (let act = 1; act <= 5; act++) {
+            fileSpec.groups.forEach(group => {
+                const vendor = config[`a${act}${group}Vendor`];
+
+                if (vendor == "None") {
+                    return;
+                }
+
+                if (!vendors[vendor]) {
+                    vendors[vendor] = [];
+                }
+
+                vendors[vendor].push(group);
             });
         }
 
-        vendor.items.forEach(item => {
-            const row = content.rows.find(r => r["name"].trim() == item);
+        if (config["clearExistingSales"]) {
+            Object.keys(vendors).forEach(vendor => {
+                content.rows.forEach(row => {
+                    row[`${vendor}Min`] = "";
+                    row[`${vendor}Max`] = "";
+                    row[`${vendor}MagicMin`] = "";
+                    row[`${vendor}MagicMax`] = "";
+                });
+            });
+        }
 
-            if (!row) {
-                throw new Error(`Item "${item}" not found.`);
-            }
+        Object.keys(vendors).forEach(vendor => {
+            vendors[vendor].forEach(group => {
+                ITEM_SPECS[group].forEach(item => {
+                    const row = content.rows.find(r => r["name"].trim() == item);
 
-            assignVendor(row, vendor.name);
+                    if (!row) {
+                        throw new Error(`Item "${item}" not found.`);
+                    }
+
+                    row["spawnable"] = 1;
+
+                    if (config["baseCost"] != -1) {
+                        row["cost"] = config["baseCost"];
+                    }
+
+                    row[`${vendor}Min`] = 1;
+                    row[`${vendor}Max`] = 1;
+                    row[`${vendor}MagicMin`] = 1;
+                    row[`${vendor}MagicMax`] = 1;
+                    row["PermStoreItem"] = 1;
+                    row["multibuy"] = 1;
+                });
+            });
         });
-    });
 
-    D2RMM.writeTsv(spec.filename, content);
+        D2RMM.writeTsv(filename, content);
+    });
 });
